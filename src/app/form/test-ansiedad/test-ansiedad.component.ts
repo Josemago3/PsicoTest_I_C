2d2b998b65b94d80d3b98c1c4c13daf40dc8ca68
Hugo Berra <53909003+jtberra@users.noreply.github.com>
2022-05-17 12:38:47 -0500

    ansiedadV2

--- src/app/form/test-ansiedad/test-ansiedad.component.ts
+++ src/app/form/test-ansiedad/test-ansiedad.component.ts
@@ -26,33 +26,74 @@ export class TestAnsiedadComponent{
 
   banco = [
     {
-      'pregunta':'Hubo días en los que me sentí cansado y fatigado tratando de realizar incluso las tareas o actividades físicas más pequeñas, a pesar de que estaba durmiendo lo suficiente.',
+      'pregunta':'Torpe o entumecido',
       'name' : 'campo1',
       'resp1':'id1',
       'resp2':'id2',
       'resp3':'id3',
     },
     {
-      'pregunta':'Hubo períodos en los que estaba durmiendo demasiado, y / o tenía problemas para conciliar el sueño y permanecer dormido, sin razón aparente.',
+      'pregunta':'Acalorado',
       'name' : 'campo2',
       'resp1':'id4',
       'resp2':'id5',
       'resp3':'id6',
     },
     {
-      'pregunta':'Hubo días en los que tuve problemas para seguir las conversaciones o instrucciones de otras personas porque me sentía distraído por todas las otras cosas que sucedían a mi alrededor.',
+      'pregunta':'Con temblor en las piernas',
       'name' : 'campo3',
       'resp1':'id7',
       'resp2':'id8',
       'resp3':'id9',
     },
     {
-      'pregunta':'Hubo un período de cuatro días consecutivos o más en el que me sentí triste, desesperada o vacía durante la mayor parte del día sin razón aparente.',
+      'pregunta':'Incapaz de relajarse',
       'name' : 'campo4',
       'resp1':'id10',
       'resp2':'id11',
       'resp3':'id12',
+    },
+    {
+      'pregunta':'Con temor a que ocurra lo peor',
+      'name' : 'campo5',
+      'resp1':'id13',
+      'resp2':'id14',
+      'resp3':'id15',
+    },
+    {
+      'pregunta':'Mareado, o que se le va la cabeza',
+      'name' : 'campo6',
+      'resp1':'id16',
+      'resp2':'id17',
+      'resp3':'id18',
+    },
+    {
+      'pregunta':'Con latidos del corazón fuertes y acelerados',
+      'name' : 'campo7',
+      'resp1':'id19',
+      'resp2':'id20',
+      'resp3':'id21',
+    },
+    {
+      'pregunta':'Inestable',
+      'name' : 'campo8',
+      'resp1':'id22',
+      'resp2':'id23',
+      'resp3':'id24',
+    },
+    {
+      'pregunta':'Atemorizado o asustado',
+      'name' : 'campo9',
+      'resp1':'id25',
+      'resp2':'id26',
+      'resp3':'id27',
+    },
+    {
+      'pregunta':'Nervioso',
+      'name' : 'campo10',
+      'resp1':'id28',
+      'resp2':'id29',
+      'resp3':'id30',
     }
   ]
-
 }
